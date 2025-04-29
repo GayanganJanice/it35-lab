2aa223c974f20614b5bc33e88a0f76a002bf19d5
GayanganJanice <janicegayangan@gmail.com>
2025-04-29 16:20:02 +0800

    Updates

--- assets/ios.transition-legacy-CwHucFGE.js
+++ assets/ios.transition-legacy-CwHucFGE.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DfTqAMAg.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-DztGo6cq.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
