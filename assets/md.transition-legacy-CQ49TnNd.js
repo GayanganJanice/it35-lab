221c9e354561fd720a4cdc0acb01f1a5c7c0fbce
jemjem7 <gitgakko@gmail.com>
2025-04-30 06:52:25 +0800

    Updates

--- assets/md.transition-legacy-CQ49TnNd.js
+++ assets/md.transition-legacy-CQ49TnNd.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DztGo6cq.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-hXedMLG4.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
